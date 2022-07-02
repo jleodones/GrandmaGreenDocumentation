2b41ca14b44ad617fb26148f60ca47801fe969f2
Jamie Leodones <leodones@usc.edu>
2022-07-02 22:34:38 +0000

    Commit from GitHub Actions (Sync with Perforce.)

---
+++ Scripts/Save/SaveManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GrandmaGreen.SaveSystem
+{
+    public class SaveManager : MonoBehaviour
+    {
+        
+    }   
+}
